e2913be44c136bab5f6b3c4c59943bde26275773
javier.pueyo.serrano <javier.pueyo.serrano@alumnos.upm.es>
2023-10-06 12:39:08 +0200

    Milestone 1 Updates

--- Milestone_1/Milestone_1.py
+++ Milestone_1/Milestone_1.py
@@ -9,11 +9,9 @@
 #=================================================================================
 
 
-
 #=================================================================================
 #								      HITO 1
 #=================================================================================
-
 
 
 #=================================================================================
@@ -39,9 +37,15 @@
 # Se puede ver que en este caso no hay una dependencia explícita con el tiempo. Al vector U se le denomina vector de estado.
 #
 
+from numpy import array, zeros, linspace
+import matplotlib.pyplot as plt
+from scipy.optimize import newton
 
-#from numpy import array 
-
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Computer Modern Roman"],
+})
 
 print( "" )
 print( "   ==========================================================================" )
@@ -56,7 +60,7 @@ print( "" )
 print( "" )
 print( "" )
 print( "   ==========================================================================" )
-print( "   ==============================    HITO 1    ==============================" )
+print( "   ============================    MILESTONE 1    ===========================" )
 print( "   ==========================================================================" )
 print( "" )
 print( "   Problema de Kepler: ")
@@ -75,7 +79,21 @@ print( "" )
 eleccion = int(input("   Introduzca la opción elegida: "))
 print( "" )
 
-def Ec_Kepler(U, t):
+U0 = array( [ 1, 0, 0, 1 ] )
+N = int(input("   Introduzca N = "))
+dt = float(input("   Introduzca delta_t = "))
+U = array( zeros ( [ N , len(U0) ] ) ) 
+U[0,:] = U0
+
+x = array( zeros(N) )
+y = array( zeros(N) )
+t = array( zeros(N) )
+
+x[0] = U[0,0]
+y[0] = U[0,1]
+t[0] = 0
+
+def Ec_Kepler( U, t ):
 
 	x = U[0]			# La coordenada x es la primera componente del vector de estado.
 	y = U[1]			# La coordenada y es la segunda componente del vector de estado.
@@ -86,31 +104,21 @@ def Ec_Kepler(U, t):
 
 	return array( [ dx_dt , dy_dt , -x/mod_r**3 , -y/mod_r**3 ] )
 
+
 if eleccion == 1:
 
-	print("   Ha seleccionado el Método de Euler.")
+	print("   Ha seleccionado el Método de Euler (explícito).")
 	input()
-	U = array( [ 1, 0, 0, 1])
-
-	N = int(input("   Introduzca N = "))
-	dt = float(input("   Introduzca delta_t = "))
-
-	x = array( zeros(N) )
-	y = array( zeros(N) )
-	t = array( zeros(N) )
-
-	x[0] = U[0]
-	y[0] = U[1]
-	t[0] = 0
 
 	for i in range(1, N):
 
 		t[i] = dt * i
-		U = Euler_Exp(U, t, dt, Ec_Kepler)
-		x[i] = U[0]
-		y[i] = U[1]
+		F = Ec_Kepler( U = U[i-1,:] , t = t[i-1])
+		U[i,:] = U[i-1,:] + dt * F
+		x[i] = U[i,0]
+		y[i] = U[i,1]
 
-	plt.figure( figsize=(6, 6) , dpi=80 , num=3)
+	plt.figure( figsize=(6, 6) , dpi=80 , num=2)
 	plt.grid( axis = 'both' , color = 'gainsboro' , linestyle = 'solid' )
 	plt.ylim( [ -1.15 , 1.15  ])
 	plt.xlim( [ -1.15 , 1.15 ] )
@@ -118,23 +126,66 @@ if eleccion == 1:
 	plt.title(r'\textbf{Órbita}', loc = "center", fontdict = {'fontsize':14, 'color':'k'})
 	plt.ylabel("$y$ (m)", fontdict = {'fontsize':12, 'fontweight':'normal', 'color':'k'})
 	plt.xlabel("$x$ (m)", fontdict = {'fontsize':12, 'fontweight':'normal', 'color':'k'})
-	#plt.yticks(range(20, 35))
-	#plt.savefig('Orbit.svg')
 	plt.show()
-
+	
+	
 elif eleccion == 2:
 	
-	print("   Ha seleccionado el Método de Crank-Nicolson.")
+	print("   Ha seleccionado el Método Crank-Nicolson.")
 	input()
-	import Hito_1_CraNic
-	Hito_1_CraNic.Hito_1_CN()
+
+	def G(X):
+		return X - a - dt/2 * ( F(a,t[i-1]) + F(X,t[i]) )
+
+	for i in range(1,N):
+
+		t[i] = dt * i
+		F = Ec_Kepler
+		a = U[i-1,:]
+		U[i,:] = newton( func = G , x0 = U[i-1,:] )
+		x[i] = U[i,0]
+		y[i] = U[i,1]
+
+	plt.figure( figsize=(6, 6) , dpi=80 , num=2)
+	plt.grid( axis = 'both' , color = 'gainsboro' , linestyle = 'solid' )
+	plt.ylim( [ -1.15 , 1.15  ])
+	plt.xlim( [ -1.15 , 1.15 ] )
+	plt.plot( x , y , color = 'b')
+	plt.title(r'\textbf{Órbita}', loc = "center", fontdict = {'fontsize':14, 'color':'k'})
+	plt.ylabel("$y$ (m)", fontdict = {'fontsize':12, 'fontweight':'normal', 'color':'k'})
+	plt.xlabel("$x$ (m)", fontdict = {'fontsize':12, 'fontweight':'normal', 'color':'k'})
+	plt.show()
+
 
 elif eleccion == 3:
 	
 	print("   Ha seleccionado el Método Runge-Kutta de 4 etapas.")
 	input()
-	import Hito_1_RK4
-	Hito_1_RK4.Hito_1_RK4()
+
+	for i in range(1, N):
+
+		t[i] = dt * i
+		F = Ec_Kepler
+
+		k1 = F( U[i-1,:] , t[i-1])
+		k2 = F( U[i-1,:] + dt * k1/2 , t[i-1] + dt/2 )
+		k3 = F( U[i-1,:] + dt * k2/2 , t[i-1] + dt/2 )
+		k4 = F( U[i-1,:] + dt * k3 ,   t[i-1] + dt   )
+		U[i,:] = U[i-1,:] + dt * ( k1 + 2*k2 + 2*k3 + k4 ) / 6
+
+		x[i] = U[i,0]
+		y[i] = U[i,1]
+
+	plt.figure( figsize=(6, 6) , dpi=80 , num=2)
+	plt.grid( axis = 'both' , color = 'gainsboro' , linestyle = 'solid' )
+	plt.ylim( [ -1.15 , 1.15  ])
+	plt.xlim( [ -1.15 , 1.15 ] )
+	plt.plot( x , y , color = 'b')
+	plt.title(r'\textbf{Órbita}', loc = "center", fontdict = {'fontsize':14, 'color':'k'})
+	plt.ylabel("$y$ (m)", fontdict = {'fontsize':12, 'fontweight':'normal', 'color':'k'})
+	plt.xlabel("$x$ (m)", fontdict = {'fontsize':12, 'fontweight':'normal', 'color':'k'})
+	plt.show()
+
 
 else:
 
